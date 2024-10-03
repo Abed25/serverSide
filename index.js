4772fd78e460573e84a4c59e53500063f84812ae
Abed25 <officialwekesa254@gmail.com>
2024-10-03 15:43:46 +0300

    added a simple route

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World! This is your Express app.');
 });
 
+app.get('/abed', (req, res) => {
+  res.send('Am the author of this engine.');
+});
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
