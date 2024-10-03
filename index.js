aa6e03396244f39c6a1154d2db710e259fb2683c
Abed25 <officialwekesa254@gmail.com>
2024-10-03 16:30:22 +0300

    code formatted with prettier

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Simple route
-app.get('/', (req, res) => {
-  res.send('Hello World! This is your Express app.');
+app.get("/", (req, res) => {
+  res.send("Hello World! This is your Express app.");
 });
 
-app.get('/abed', (req, res) => {
-  res.send('Am the author of this engine.');
+app.get("/abed", (req, res) => {
+  res.send("Am the author of this engine.");
 });
-
 
 // Start the server
 app.listen(port, () => {
