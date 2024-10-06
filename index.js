7d9f54b3e2982ce1da3577b89e9c7a8cdf6ae416
Abed25 <officialwekesa254@gmail.com>
2024-10-06 09:19:36 +0300

    cloud db added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config();
+const db = require("./utils/database");
+
+//app.use(loggingMiddleware);
 
 // Simple route
 app.get("/", (req, res) => {
@@ -9,6 +13,15 @@ app.get("/", (req, res) => {
 
 app.get("/abed", (req, res) => {
   res.send("Am the author of this engine.");
+});
+
+app.get("/users", (req, res) => {
+  db.query("SELECT * FROM sampleData", (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(results);
+  });
 });
 
 // Start the server
